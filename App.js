ec348164aa10058e3b5595b86e870d83d5840664
Leya Leya <liliatolstyh@yandex.ru>
2020-08-14 12:20:07 +0300

    adding files

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './normalize.css';
+import './style.css';
+
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Кошелек</h1>
+      </header>
+      <main>Главный блок</main>
+    </>
+  );
+}
+
+export default App;
